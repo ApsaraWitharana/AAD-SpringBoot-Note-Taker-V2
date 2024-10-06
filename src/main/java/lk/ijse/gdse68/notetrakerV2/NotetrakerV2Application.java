aa373a8f420639fbf3d8266c58cf997748c71364
ApsaraWitharana <asachini095@gmail.com>
2024-10-06 12:04:28 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse68/notetrakerV2/NotetrakerV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.notetrakerV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetrakerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetrakerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse68/notetrakerV2/NotetrakerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.notetrakerV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetrakerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
