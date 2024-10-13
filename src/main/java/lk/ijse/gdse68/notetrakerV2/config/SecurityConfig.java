21c59ca948ea1a0aaad7c9f7ac342b51ebfcaec7
ApsaraWitharana <asachini095@gmail.com>
2024-10-13 13:27:21 +0530

    Add - JWT-Token SecurityConfig - auth manager

--- src/main/java/lk/ijse/gdse68/notetrakerV2/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse68/notetrakerV2/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package lk.ijse.gdse68.notetrakerV2.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,4 +36,21 @@ public class SecurityConfig {
      //restfull -section ex
         return http.build();
 }
+    @Bean
+     public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); //decrypt password encode
+     }
+// Auth provide
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider dap = new DaoAuthenticationProvider(); // user email base krl db eken eliyt gnnw
+        dap.setUserDetailsService(); //db eken user wa gnna
+        dap.setPasswordEncoder(passwordEncoder()); //
+        return dap;
+    }
+//Auth Manager - authenticate object manage krnwa
+    @Bean
+   public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+   }
 }
